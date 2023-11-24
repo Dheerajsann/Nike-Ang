8f76dafa834679d1908a4380d915bbc6646207ae
Dheeraj Sheeroju <dheerajsheeroju@gmail.com>
2023-11-24 18:26:52 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NikeComponent } from './nike/nike.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NikeComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/nike/nike.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-nike',
+  templateUrl: './nike.component.html',
+  styleUrl: './nike.component.css',
+})
+export class NikeComponent {}
